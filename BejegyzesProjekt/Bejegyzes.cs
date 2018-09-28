555dd2ccc51442e18898be98d10d850ea6899a4d
Petrik Lajos Szg <info@petrik.hu>
2018-09-28 16:25:30 +0200

    Eddig jutottam

---
+++ BejegyzesProjekt/Bejegyzes.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BejegyzesProjekt
+{
+    class Bejegyzes
+    {
+
+        public string szerzo, tartalom;
+        public DateTime letrejott, szerkesztve;
+        public int likeok;
+
+        /// <summary>
+        /// Bejegyzes ctor
+        /// </summary>
+        /// <param name="szerzo"></param>
+        /// <param name="tartalom"></param>
+        public Bejegyzes(string szerzo, string tartalom)
+        {
+            this.likeok = 0;
+            this.letrejott = DateTime.Now;
+            this.szerzo = szerzo;
+            this.tartalom = tartalom;
+            this.szerkesztve = letrejott;
+        }
+
+        /// <summary>
+        /// szerzo get
+        /// </summary>
+        public string Szerzo
+        {
+            get
+            {
+                return this.szerzo;
+            }
+        }
+
+        /// <summary>
+        /// tartalom get/set
+        /// </summary>
+        public string Tartalom
+        {
+            get
+            {
+                return this.tartalom;
+            }
+            set
+            {
+                if (this.tartalom != value)
+                {
+                    this.szerkesztve = DateTime.Now;
+                }
+            }
+        }
+
+        /// <summary>
+        /// likok get/set
+        /// </summary>
+        public int Likeok
+        {
+            get
+            {
+                return this.likeok;
+            }
+        }
+
+
+        /// <summary>
+        /// letrejott get
+        /// </summary>
+        public DateTime Letrejott
+        {
+            get
+            {
+                return this.letrejott;
+            }
+        }
+
+
+        /// <summary>
+        /// szerkesztve get
+        /// </summary>
+        public DateTime Szerkesztve
+        {
+            get
+            {
+                return this.szerkesztve;
+            }
+        }
+
+        /// <summary>
+        /// likeok számának növelése
+        /// </summary>
+        public void Like()
+        {
+            likeok++;
+        }
+
+
+        public string Kiir()
+        {
+            if (Convert.ToDateTime(letrejott) == Convert.ToDateTime(szerkesztve))
+            {
+                return "Szerző : " + szerzo + "\nTartalom : " + tartalom + "\nLikeok : " + likeok + "\nLétrejött : " + letrejott+"\n";
+            }
+            else
+            {
+                return "Szerző : " + szerzo + "\nTartalom : " + tartalom + "\nLikeok : " + likeok + "\nLétrejött : " + letrejott + "\nSzerkesztve : " + szerkesztve +"\n";
+            }
+
+        }
+
+
+    }
+}
---
+++ BejegyzesProjekt/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BejegyzesProjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bejegyzes = new Bejegyzes("Jenő", "Jenő tartalma! lel");
+            List<Bejegyzes> bejegyzesek = new List<Bejegyzes>();
+            bejegyzesek.Add(bejegyzes);
+            Console.Write("Kérem adja meg, hány bejegyzést akar létrehozni : ");
+            int db = Convert.ToInt32(Console.ReadLine());
+            string listSzerzo, listTartalom;
+            int lepteto;
+            Random rnd = new Random();
+            //StreamReader olvas = new StreamReader("bejegyzesek.txt");
+
+            do
+            {
+                Console.Write("Kérem a szerzőt : ");
+                listSzerzo = Console.ReadLine();
+                Console.Write("Kérem a bejegyzést : ");
+                listTartalom = Console.ReadLine();
+                bejegyzesek.Add(new Bejegyzes(listSzerzo, listTartalom));
+                db--;
+            } while (db != 0);
+            Console.WriteLine();
+            for (int i = 0; i < bejegyzesek.Count; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + bejegyzesek[i].Kiir());
+            }
+
+            Console.WriteLine("Bejegyzés módosításához írja be a bejegyzés számát!");
+            int szerzo = Convert.ToInt32(Console.ReadLine()) - 1;
+            if (bejegyzesek.Contains(bejegyzesek[szerzo]))
+            {
+                Console.WriteLine(bejegyzesek[szerzo].tartalom);
+                Console.WriteLine();
+                Console.WriteLine("Kérem írja át a tartalmat : ");
+                bejegyzesek[szerzo].tartalom = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Ilyen indexű szerző nem létezik!");
+            }
+
+            lepteto = bejegyzesek.Count * 20;
+            do
+            {
+                bejegyzesek[rnd.Next(bejegyzesek.Count)].Like();
+                lepteto--;
+            } while (lepteto > 0);
+
+            
+
+            for (int i = 0; i < bejegyzesek.Count; i++)
+            {
+                
+            }
+
+            foreach (var item in bejegyzesek)
+            {
+                Console.WriteLine(item.Kiir());
+            }
+
+            bejegyzes.Kiir();
+            Console.ReadKey();
+        }
+    }
+}
